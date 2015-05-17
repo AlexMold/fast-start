77c1921c547be96a2530f61a5712c6b8edea567e
AlexMold <mangol72@gmail.com>
2015-05-17 12:59:24 +0300

    optimized gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,13 @@ var gulp = require('gulp'),
     concatCss = require('gulp-concat-css'),
     minifyCss = require('gulp-minify-css'),
     autoprefixer = require('gulp-autoprefixer'),
+    concat = require('gulp-concat'),
     less = require('gulp-less');
  
 gulp.task('default', function () {
   gulp.src('css/*.less')
-    .pipe(less(""))
-    .pipe(concatCss("bundle.css"))
+    .pipe(concat("bundle.less"))
+    .pipe(less("bundle.css"))
     .pipe(autoprefixer('> 1%', 'last 5 versions', 'Firefox ESR', 'ie 9'))
     .pipe(minifyCss(""))
     .pipe(gulp.dest('./'));
